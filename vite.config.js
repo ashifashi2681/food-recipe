8aa5164c272165105e28034282d9fae63e74cdf9
ashifashi2681 <ashifashi2681@gmail.com>
2024-06-04 21:02:54 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/food-recipe/",
-  plugins: [react()],
-})
+	base: "/food-recipe/",
+	plugins: [react()],
+});
